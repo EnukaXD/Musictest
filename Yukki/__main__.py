a10c2e15d2d72be90d3fdd9198a7c74e32fe6bd0
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-02-19 14:50:18 +0530

    Update __main__.py

--- Yukki/__main__.py
+++ Yukki/__main__.py
@@ -2,8 +2,10 @@ import asyncio
 import importlib
 import os
 import re
+from Yukki.Core.Clients.cli import LOG_CLIENT
 import random
 
+from config import LOG_GROUP_ID
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pytgcalls import idle
